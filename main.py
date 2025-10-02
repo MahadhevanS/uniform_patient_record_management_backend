a9b224b6b02143c6837b309587b207606eff92b7
MahadhevanS <mahadhevan0502@gmail.com>
2025-10-02 13:38:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5176",  # Your React/Vite frontend URL
     "http://127.0.0.1:5176",
+    "https://uniformpatientrecord.netlify.app",
     "https://*.netlify.app",
 ]
 
